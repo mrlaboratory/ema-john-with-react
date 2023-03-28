087f3e4dd4c74dd0a2daca9cce1ab0d4fdf3185d
mrlaboratory <mrlaboratory23@gmail.com>
2023-03-28 08:15:35 +0600

    complate the cart calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
 import Product from './components/Product/Product';
+import { addToDb, getShoppingCart } from './utilities/fakedb';
 
 const App = () => {
   const [data, setData] = useState([])
+  const [product, setProduct] = useState([])
+  const [cartData, setCartData] = useState([])
 
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
       .then(res => res.json()).then(data => setData(data))
   }, [])
-  console.log(data[0])
+
+  const shopingCrt = getShoppingCart()
+  useEffect(() => {
+    const savedCart = []
+      for (const id in shopingCrt) {
+        const addedProduct = data.find(item => item.id === id)
+        if(addedProduct){
+          addedProduct.quantity = shopingCrt[id]
+          savedCart.push(addedProduct)
+        }
+    }
+    setCartData(savedCart)
+  }, [data,product])
+
+
+  const addToCart = (data) => {
+    addToDb(data.id)
+    const newdata = [...cartData, data]
+    setProduct(newdata)
+
+  }
+  // console.log(data[0])
+  const toTalPrice = cartData.reduce((p, c) => p + (c.price*c.quantity), 0)
+  const tax = toTalPrice*7 / 100
+  const grandTotal = toTalPrice - tax
   return (
     <div className='container mx-auto'>
       <Header></Header>
       <div className='grid grid-cols-3 md:grid-cols-4 mt-10 gap-3'>
         <div className='products-container col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3'>
-          
+
 
           {
-            data.map((product, i) => <Product key={i} {...product}></Product>)
+            data.map((product, i) => <Product key={i} addToCart={addToCart} {...product}></Product>)
           }
         </div>
-        <div className='product-cart'>
-          <h2>cart</h2>
+        <div className='product-cart h-screen sticky top-0 bg-[#FFE0B3]'>
+          <h2 className='text-2xl text-center border-b-4 border-black'>Your Shopping list</h2>
+          <h3 className='text-2xl text-center'>Total Product : {cartData.reduce((p,c) => p+c.quantity,0)}</h3>
+          <h3 className='text-2xl text-center'>Total Price : ${toTalPrice}</h3>
+          <h2 className='text-2xl text-center'>Shipping : ${cartData.reduce((p,c) => p+c.shipping,0)}</h2>
+          <h2 className='text-2xl text-center'>Tax : $ {tax.toFixed(2)}</h2>
+
+          <h2 className='text-2xl text-center font-bold'>Grand Total : {grandTotal.toFixed(2)}</h2>
+
 
         </div>
 
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-const Product = ({ img, name, price,seller,ratings }) => {
-    console.log(img)
+const Product = (props) => {
+const { img, name, price,seller,ratings, addToCart } = props
+    // console.log(functioncheck)
     return (
         <div className='border rounded-lg relative '>
             <img className='w-[100%] h-auto p-3 rounded-lg' src={img} alt="" />
@@ -13,7 +14,7 @@ const Product = ({ img, name, price,seller,ratings }) => {
                 <p>Rating : {ratings} Stars</p>
 
             </div>
-            <button className='btn border-none text-black hover:text-white bg-[#FFE0B3] absolute bottom-0 w-[100%] py-2 rounded-lg'>Add to cart  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <button onClick={() => addToCart(props)}  className='btn border-none text-black hover:text-white bg-[#FFE0B3] absolute bottom-0 w-[100%] py-2 rounded-lg'>Add to cart  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
 </svg>
 
